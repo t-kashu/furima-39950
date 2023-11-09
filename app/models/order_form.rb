70f9587d35cebde8b13822b07e1aaba15626dace
t-kashu <146803701+t-kashu@users.noreply.github.com>
2023-11-09 15:34:11 +0900

    商品購入機能実装修正1

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,15 +2,18 @@ class OrderForm
   include ActiveModel::Model
  
   attr_accessor :user_id, :item_id, :postal_code, :item_prefecture_id, :city, :addresses, :building, :phone_number, :token
-  validates :user_id,                  presence: true
-  validates :item_id,                  presence: true
-  validates :postal_code,              presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-  validates :item_prefecture_id,       presence: true, numericality: { other_than: 1, message: "Prefecture can't be blank" }
-  validates :city,                     presence: true
-  validates :addresses,                presence: true
-  validates :phone_number,             presence: true, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
-  validates :token,                    presence: true
- 
+
+  with_options presence: true do
+  validates :user_id
+  validates :item_id
+  validates :postal_code,              format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+  validates :item_prefecture_id,       numericality: { other_than: 1, message: "Prefecture can't be blank" }
+  validates :city
+  validates :addresses
+  validates :phone_number,             format: { with: /\A\d{10}\z|\A\d{11}\z/, message: 'is invalid' }
+  validates :token
+  end
+  
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     shipping_address = ShippingAddress.create(postal_code: postal_code, item_prefecture_id: item_prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -23,38 +23,51 @@ RSpec.describe OrderForm, type: :model do
       it '郵便番号が「3桁ハイフン4桁」の半角文字列でなければ保存できない' do
         @order_form.postal_code = ''
         @order_form.valid?
-        expect(@order_form.errors.full_messages)
+        expect(@order_form.errors.full_messages).to include("Postal code can't be blank", "Postal code is invalid. Enter it as follows (e.g. 123-4567)")
       end
 
       it '都道府県が空では保存できない' do
         @order_form.item_prefecture_id = ''
         @order_form.valid?
-        expect(@order_form.errors.full_messages)
+        expect(@order_form.errors.full_messages).to include("Item prefecture can't be blank", "Item prefecture Prefecture can't be blank")
       end
 
       it '市区町村が空では保存できない' do
         @order_form.city = ''
         @order_form.valid?
-        expect(@order_form.errors.full_messages)
+        expect(@order_form.errors.full_messages).to include("City can't be blank")
       end
 
       it '番地が空では保存できない' do
         @order_form.addresses = ''
         @order_form.valid?
-        expect(@order_form.errors.full_messages)
+        expect(@order_form.errors.full_messages).to include("Addresses can't be blank")
       end
 
       it '電話番号が空では保存できない' do
         @order_form.phone_number = ''
         @order_form.valid?
-        expect(@order_form.errors.full_messages)
+        expect(@order_form.errors.full_messages).to include("Phone number can't be blank", "Phone number is invalid")
       end
 
       it '電話番号は、10桁以上11桁以内の半角数値でなければ保存できない' do
         @order_form.phone_number = '123-４５６７-8901'
         @order_form.valid?
-        expect(@order_form.errors.full_messages)
+        expect(@order_form.errors.full_messages).to include("Phone number is invalid")
       end
+
+      it '9桁以下の電話番号で保存できない' do
+        @order_form.phone_number = '090'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include('Phone number is invalid')
+      end
+
+      it '12桁以上の電話番号で保存できない' do
+        @order_form.phone_number = '1234567890987'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include('Phone number is invalid')
+      end
+
     end
   end
 end
